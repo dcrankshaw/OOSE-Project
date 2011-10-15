ebcf757c05fd3e30383a6e112783f42e5d7eea58
Dan Crankshaw <dscrankshaw@gmail.com>
2011-10-14 22:35:47 -0400

    Updated javadocs, fixed bug with javadoc index.html

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/FileDisplayPanel.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/FileDisplayPanel.java
@@ -3,6 +3,8 @@ package edu.jhu.cs.oose.biblio.gui;
 import javax.swing.JMenu;
 import javax.swing.JPanel;
 
+import edu.jhu.cs.oose.biblio.model.FileMetadata;
+
 /**
  * A GUI elements that provides a user interface for the underlying
  * file
