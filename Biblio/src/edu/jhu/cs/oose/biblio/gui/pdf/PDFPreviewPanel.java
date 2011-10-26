e4d91240151558a3cef0ae956b7a37a1f6ff9958
Paul O'Neil <redballoon36@gmail.com>
2011-10-26 18:44:39 -0400

    Defined the preferred size of a PDFPreviewPanel to be the size of the pdf thumbnail

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/pdf/PDFPreviewPanel.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/pdf/PDFPreviewPanel.java
@@ -1,5 +1,10 @@
 package edu.jhu.cs.oose.biblio.gui.pdf;
 
+import java.awt.Dimension;
+import java.awt.Image;
+
+import org.jpedal.exception.PdfException;
+
 import edu.jhu.cs.oose.biblio.gui.PreviewPanel;
 import edu.jhu.cs.oose.biblio.model.pdf.PDFFileContents;
 
@@ -12,12 +17,38 @@ import edu.jhu.cs.oose.biblio.model.pdf.PDFFileContents;
 public class PDFPreviewPanel extends PreviewPanel {
 	
 	/** The contents of the file we are showing a preview of */
-	public PDFFileContents PDFDocument;
+	public PDFFileContents contents;
 
+	/**
+	 * Creates a new PDFPreviewPanel with no file to display.
+	 */
+	public PDFPreviewPanel()
+	{
+		contents = null;
+	}
 	
-	/** The method to actually draw the file Preview */
+	/**
+	 * Creates a new PDFPreviewPanel that displays a preview
+	 * of the given file contents
+	 * @param contents the file contents to preview
+	 */
+	public PDFPreviewPanel(PDFFileContents c) {
+		contents = c;
+	}
+	
 	@Override
-	public void displayPreview() {}
-	
-
+	public Dimension getPreferredSize()
+	{
+		try {
+			if( contents != null ) {
+				Image preview = contents.getThumbnail();
+				return new Dimension(preview.getWidth(null), preview.getHeight(null));
+			}
+		}
+		catch(PdfException e) {
+			System.err.println("Could not generate the thumbnail needed for a preview: " + e.getLocalizedMessage());
+		}
+		// TODO this should probably be something sensible, and not magic numbers
+		return new Dimension(10, 10);
+	}
 }
