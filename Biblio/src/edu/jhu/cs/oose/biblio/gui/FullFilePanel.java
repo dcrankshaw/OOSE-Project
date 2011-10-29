04eda293c4f76a10d6c2b193125d66e56e759343
Paul O'Neil <redballoon36@gmail.com>
2011-10-29 00:34:27 -0400

    Redefined FullFilePanel to be the panel INSIDE the scrollbars

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/FullFilePanel.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/FullFilePanel.java
@@ -1,9 +1,53 @@
 package edu.jhu.cs.oose.biblio.gui;
 
-/**
- * Displays the entire file, drawing scrollbars, etc needed to view the whole file, depending what type of file it is.
- */
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
-public abstract class FullFilePanel extends FileDisplayPanel {
+import javax.swing.JPanel;
+import javax.swing.JViewport;
+import javax.swing.Scrollable;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * Displays the entire file, not including scrollbars, etc needed to view the whole file
+ * depending what type of file it is.
+ */
+public abstract class FullFilePanel extends JPanel implements ChangeListener, Scrollable {
 	
+	/**
+	 * The viewport containing this panel.  We need
+	 * access to this in order to find out which part
+	 * of ourself to draw.
+	 */
+	private JViewport viewport;
+	
+	/**
+	 * This overrides JPanel.paint() to paint with the additional
+	 * knowledge of which part of the panel is visible.
+	 */
+	@Override
+	public final void paint(Graphics g) {
+		paint(g, viewport.getViewRect());
+	}
+	
+	/**
+	 * Paints only the region that is visible
+	 * @param g the graphics context to draw in
+	 * @param region the visible region to draw in
+	 */
+	public abstract void paint(Graphics g, Rectangle region);
+	
+	/**
+	 * Sets the parent viewport of this panel
+	 * @param view the parent viewport
+	 */
+	public void setViewport(JViewport view) {
+		viewport = view;
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent o) {
+		repaint();
+	}
 }
