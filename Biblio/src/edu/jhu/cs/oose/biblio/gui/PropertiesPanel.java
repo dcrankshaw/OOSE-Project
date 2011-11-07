217711e4bf27c9198972e2976f7d86529fb7c6f2
Paul O'Neil <redballoon36@gmail.com>
2011-11-06 23:41:27 -0500

    Wrote the properties panel

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/PropertiesPanel.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/PropertiesPanel.java
@@ -1,5 +1,9 @@
 package edu.jhu.cs.oose.biblio.gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.text.DateFormat;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -25,10 +29,46 @@ public class PropertiesPanel extends JPanel {
 	 * A label displaying the date and time of the last time this file was opened in Biblio
 	 */
 	private JLabel lastOpenedLabel;
+	private JLabel lastOpenedTitle;
 	
 	/**
 	 * A label displaying the number of times this file has been opened through Biblio
 	 */
 	private JLabel openedCountLabel;
+	private JLabel openedCountTitle;
 	
+	DateFormat formatter;
+	
+	public PropertiesPanel() {
+		this.tagsListPane = new TagsListPanel();
+		this.lastOpenedTitle = new JLabel("Last Opened:");
+		this.lastOpenedLabel = new JLabel();
+		this.openedCountTitle = new JLabel("Opened Count:");
+		this.openedCountLabel = new JLabel();
+		
+		JPanel twoColumns = new JPanel();
+		twoColumns.setLayout(new GridLayout(2, 2));
+		twoColumns.add(this.lastOpenedTitle);
+		twoColumns.add(this.lastOpenedLabel);
+		twoColumns.add(this.openedCountTitle);
+		twoColumns.add(this.openedCountLabel);
+		
+		this.setLayout(new BorderLayout());
+		this.add(twoColumns, BorderLayout.NORTH);
+		this.add(tagsListPane, BorderLayout.CENTER);
+		
+		formatter = DateFormat.getDateInstance();
+	}
+	
+	public PropertiesPanel(FileMetadata f) {
+		this();
+		setFile(f);
+	}
+	
+	public void setFile(FileMetadata f) {
+		this.file = f;
+		lastOpenedLabel.setText(formatter.format(this.file.getLastOpened()));
+		openedCountLabel.setText(Integer.toString(this.file.getOpenedCount()));
+		tagsListPane.setFile(this.file);
+	}
 }
