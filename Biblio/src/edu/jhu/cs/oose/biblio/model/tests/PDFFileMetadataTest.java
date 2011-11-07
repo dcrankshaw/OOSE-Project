d5e98023eb3f3fbbea376e02e3ea9b5cf8534990
Dan Crankshaw <dscrankshaw@gmail.com>
2011-11-06 22:35:37 -0500

    Full text searching passes basic unit tests

--- Biblio/src/edu/jhu/cs/oose/biblio/model/tests/PDFFileMetadataTest.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/model/tests/PDFFileMetadataTest.java
@@ -1,11 +1,31 @@
 package edu.jhu.cs.oose.biblio.model.tests;
 
+import java.io.File;
+import java.util.Date;
+import java.util.TreeSet;
+
 import junit.framework.TestCase;
+import edu.jhu.cs.oose.biblio.model.Tag;
+import edu.jhu.cs.oose.biblio.model.pdf.PDFFileMetadata;
+
 
 public class PDFFileMetadataTest extends TestCase {
 
+	PDFFileMetadata searchTestFile;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
+		String path = "testfiles/test1.pdf";
+		File f = new File(path);
+		if(f.exists())
+		{
+			searchTestFile = new PDFFileMetadata(new Date(), 0, path, new TreeSet<Tag>());
+		}
+		else
+		{
+			searchTestFile = null;
+		}
+		
 	}
 
 	public void testGetContents() {
@@ -13,7 +33,25 @@ public class PDFFileMetadataTest extends TestCase {
 	}
 
 	public void testSearchText() {
-		fail("Not yet implemented");
+		String search1 = "Gildas"; //String occurs once in document
+		String search0 = "kjsdhfkjdsh"; //String occurs 0 times
+		String searchMany = "Maxwell";
+		assertFalse(searchTestFile == null);
+		try
+		{
+			int results1 = searchTestFile.searchText(search1);
+			assertEquals(1, results1);
+			assertEquals(0, searchTestFile.searchText(search0));
+			int resultsMany = searchTestFile.searchText(searchMany);
+			assertTrue(resultsMany > 10);
+			System.out.println(resultsMany);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			fail("Threw unexpected exception");
+		}
 	}
 
 }
