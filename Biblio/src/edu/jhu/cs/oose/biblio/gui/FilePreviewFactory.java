fccb7414028c5006ce8a616223d857a5cdfde89d
Paul O'Neil <redballoon36@gmail.com>
2011-11-09 22:14:11 -0500

    Javadoc-ed FileImportCell, FilePreviewFactory, and Bookmark

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/FilePreviewFactory.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/FilePreviewFactory.java
@@ -5,8 +5,19 @@ import edu.jhu.cs.oose.biblio.model.FileMetadata;
 import edu.jhu.cs.oose.biblio.model.UnsupportedFiletypeException;
 import edu.jhu.cs.oose.biblio.model.pdf.PDFFileMetadata;
 
+/**
+ * Factory to create FilePreviewPanels.
+ */
 public class FilePreviewFactory {
 
+	/**
+	 * Creates a FilePreviewPanel capable of displaying the contents
+	 * of the given file.
+	 * @param file the file to preview
+	 * @return a new FilePreviewPanel that displays the file
+	 * @throws UnsupportedFiletypeException if the factory does not know how to handle
+	 *  the file
+	 */
 	public static PreviewPanel createPreview(FileMetadata file) throws UnsupportedFiletypeException
 	{
 		// Zach says to use the visitor pattern here
