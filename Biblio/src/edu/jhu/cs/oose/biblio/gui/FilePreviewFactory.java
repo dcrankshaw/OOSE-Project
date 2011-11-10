5ffca705838a356ecf691602216a0de144a986d6
Paul O'Neil <redballoon36@gmail.com>
2011-11-09 21:38:34 -0500

    Moved libraries, got rid of an import

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/FilePreviewFactory.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/FilePreviewFactory.java
@@ -3,7 +3,6 @@ package edu.jhu.cs.oose.biblio.gui;
 import edu.jhu.cs.oose.biblio.gui.pdf.PDFPreviewPanel;
 import edu.jhu.cs.oose.biblio.model.FileMetadata;
 import edu.jhu.cs.oose.biblio.model.UnsupportedFiletypeException;
-import edu.jhu.cs.oose.biblio.model.pdf.PDFFileContents;
 import edu.jhu.cs.oose.biblio.model.pdf.PDFFileMetadata;
 
 public class FilePreviewFactory {
