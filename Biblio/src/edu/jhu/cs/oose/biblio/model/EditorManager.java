5d751f64d05747baa7682257aa9111d8ee58b79f
Dan Crankshaw <dscrankshaw@gmail.com>
2011-11-16 14:55:50 -0500

    Added an EditorManager class

---
+++ Biblio/src/edu/jhu/cs/oose/biblio/model/EditorManager.java
@@ -0,0 +1,11 @@
+package edu.jhu.cs.oose.biblio.model;
+
+import java.util.Set;
+
+public class EditorManager {
+	
+	public Set<Tag> getAllTags(){}
+	
+	public Set<Category> getAllCategories(){}
+
+}
