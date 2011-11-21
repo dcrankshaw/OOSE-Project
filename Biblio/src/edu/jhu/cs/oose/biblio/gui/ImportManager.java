a705e304ffc5d07136f1af9ead4ba31033fea597
Paul O'Neil <redballoon36@gmail.com>
2011-11-21 14:56:27 -0500

    Added a menu bar for editing tags and importing. Added a simple ImportManager, just to get the ImportDialog up with files

---
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/ImportManager.java
@@ -0,0 +1,46 @@
+package edu.jhu.cs.oose.biblio.gui;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import edu.jhu.cs.oose.biblio.model.FileMetadata;
+import edu.jhu.cs.oose.biblio.model.pdf.PDFFileMetadata;
+
+public class ImportManager {
+	public void startImportProcess(JFrame parent) {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setMultiSelectionEnabled(true);
+		fileChooser.setDialogTitle("Select the files to import");
+		int choiceResult = fileChooser.showDialog(parent, "Import");
+		if( JFileChooser.CANCEL_OPTION == choiceResult ) {
+			return;
+		}
+		else if( JFileChooser.ERROR_OPTION == choiceResult ) {
+			// TODO handle the error
+			return;
+		}
+		else if( JFileChooser.APPROVE_OPTION == choiceResult ) {
+			List<FileMetadata> files = getMetadataForFiles(fileChooser.getSelectedFiles());
+			ImportDialog importer = new ImportDialog(files, parent);
+			//importer.
+		}
+	}
+	
+	private List<FileMetadata> getMetadataForFiles(File[] paths) {
+		List<FileMetadata> result = new ArrayList<FileMetadata>(paths.length);
+		for( File f : paths ) {
+			// TODO use the MIME type library...
+			if( f.getName().endsWith(".pdf")) {
+				result.add(new PDFFileMetadata(f.getAbsolutePath()));
+			}
+			else {
+				// TODO read other kinds of files or give errors
+			}
+		}
+		return result;
+	}
+}
