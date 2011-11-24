c5f1ae46ee1d651c649338890ddd5ff8f544b726
Paul O'Neil <redballoon36@gmail.com>
2011-11-23 21:00:19 -0500

    Added Category methods to the EditorManager

--- Biblio/src/edu/jhu/cs/oose/biblio/model/EditorManager.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/model/EditorManager.java
@@ -14,6 +14,7 @@ public class EditorManager {
 		return null;
 	}
 	
+	// Can we do these with reflection or something? - Paul
 	public Tag newTag() {
 		// TODO insert this into the database
 		return new Tag();
@@ -21,6 +22,15 @@ public class EditorManager {
 	
 	public void deleteTag(Tag toRemove) {
 		// TODO remove the tag from the database
+	}
+	
+	public Category newCategory() {
+		// TODO insert this into the database
+		return new Category();
+	}
+	
+	public void deleteCategory(Category toRemove) {
+		// TODO remove this from the database
 	}
 
 }
