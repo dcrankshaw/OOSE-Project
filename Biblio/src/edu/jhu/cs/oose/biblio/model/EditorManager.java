d8ceaf51c989deef6fe1ffdf699fe095feec458d
Cain <kbronze1989@hotmail.com>
2011-11-23 23:41:31 -0500

    Implement methods in EditorManager, except for deleteTag(), deleteCategory().
    Please check the comments on the return object.
    TODO on reflection.

--- Biblio/src/edu/jhu/cs/oose/biblio/model/EditorManager.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/model/EditorManager.java
@@ -1,31 +1,55 @@
 package edu.jhu.cs.oose.biblio.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 public class EditorManager {
 	
+	private Set<Tag> tags = new HashSet<Tag>();
+	private Set<Category> categories = new HashSet<Category>();
+	
+	SessionFactory sessionFactory;//TODO private???
+	
+	@SuppressWarnings("unchecked")
 	public Set<Tag> getAllTags()
-	{
-		return null;
+	{	
+		
+		Session session = sessionFactory.getCurrentSession();
+		tags = (HashSet<Tag>) session.createQuery("from Tag").list();
+		return tags;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public Set<Category> getAllCategories()
-	{
-		return null;
+	{	
+		Session session = sessionFactory.getCurrentSession();
+		categories = (HashSet<Category>) session.createQuery("from Category").list();
+		return categories;
 	}
 	
 	// Can we do these with reflection or something? - Paul
-	public Tag newTag() {
+	public Tag newTag(String tagName) {
 		// TODO insert this into the database
-		return new Tag();
+		Tag t = new Tag(tagName);
+		Session session = sessionFactory.getCurrentSession();
+		session.save(t);
+		return new Tag();//not sure what this new tag obj. is doing here.
 	}
 	
 	public void deleteTag(Tag toRemove) {
 		// TODO remove the tag from the database
 	}
 	
-	public Category newCategory() {
+	public Category newCategory(String catName) {
 		// TODO insert this into the database
+		Category c = new Category(catName);
+		Session session = sessionFactory.getCurrentSession();
+		session.save(c);
 		return new Category();
 	}
 	
