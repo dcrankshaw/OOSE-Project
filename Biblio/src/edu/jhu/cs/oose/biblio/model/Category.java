7d1ec49239480b6ecf04010b6732e0016f0e5370
Paul O'Neil <redballoon36@gmail.com>
2011-12-04 02:01:22 -0500

    Updated Category constructors to use Database & Javadoc Make sure that Category names don't have colons

--- Biblio/src/edu/jhu/cs/oose/biblio/model/Category.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/model/Category.java
@@ -23,6 +23,7 @@ import org.hibernate.annotations.GenericGenerator;
 @Table( name = "CATEGORY" )
 public class Category implements Comparable<Category>, Keyed {
 	
+	/** The database's primary key for this Category */
 	@Id
 	@GenericGenerator(name="generator", strategy="increment")
 	@GeneratedValue(generator="generator")
@@ -46,42 +47,90 @@ public class Category implements Comparable<Category>, Keyed {
 	)
 	public Set<Tag> tags;
 	
-	public Category() {
-		super();
+	/**
+	 * Creates a new, empty Category.
+	 * This should only be used by Hibernate.
+	 * Use the other constructor instead.
+	 */
+	@SuppressWarnings("unused")
+	private Category() {
 		tags = new TreeSet<Tag>();
 	}
 	
+	/**
+	 * Creates a new Category with the given name.
+	 * It gets a primary key for the DB, so
+	 * there must be an open transaction
+	 * @param n the name of the new Category
+	 */
+	public Category(String n) {
+		this.name = n;
+		this.tags = new HashSet<Tag>();
+		
+		Database.getSessionFactory().getCurrentSession().save(this);
+		@SuppressWarnings("unchecked")
+		Database<Category> db = (Database<Category>)Database.get(Category.class);
+		db.add(this);
+	}
+	
+	@Override
 	public int getId() {
 		return id;
 	}
 	
+	/**
+	 * Sets the databases primary key for this Category.
+	 * @param id the new primary key
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Returns the name of this category.
+	 * @return the name of this category
+	 */
 	public String getName() {
 		return name;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	/**
+	 * Sets the name of this Category to the given value.  The name
+	 * may not include the ':' character, as that denotes the end
+	 * of a Category name when searching.  Attempting to set a name
+	 * with a colon will fail
+	 * @param n the new name of this Category
+	 * @return true if this is a valid Category name, or false if the name
+	 * contains a colon and is not a valid name.
+	 */
+	public boolean setName(String n) {
+		if(n.contains(":"))
+			return false;
+		else {
+			this.name = n;
+			return true;
+		}
 	}
 	
+	@Override
 	public String toString() {
 		return "Category -  ID:" + id + " NAME:" + name + "\n";
 	}
 	
+	/**
+	 * Returns the set of Tags that are in this Category
+	 * @return the set of Tags that are in this Category
+	 */
 	public Set<Tag> getTags() {
 		return tags;
 	}
 	
+	/**
+	 * Adds the given Tag to this Category
+	 * @param t the Tag to add to this Category
+	 */
 	public void addTag(Tag t) {
 		tags.add(t);
-	}
-	
-	public Category(String n) {
-		this.name = n;
-		this.tags = new HashSet<Tag>();
 	}
 	
 	@Override
