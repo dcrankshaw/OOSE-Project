1e672660ce254d7372833fd4a153c948876475e4
Jiefeng Zhai <jzhai2@gmail.com>
2011-12-07 01:33:27 -0500

    WatcherSelectorDialog.java and WatcherSelectorPanel.java

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/MainWindow.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/MainWindow.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -94,6 +97,17 @@ public class MainWindow extends JFrame {
 				tagFrame.pack();
 				tagFrame.setTitle("Manage Tags");
 				tagFrame.setVisible(true);
+			}
+		});
+		menu.add(item);
+		
+		item = new JMenuItem("Watch Directories");
+		item.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Watcher w = Watcher.getWatcher();
+				List<File> fList = new ArrayList<File>(w.getWatchedDirectories());
+				new WatcherSelectorDialog(fList, MainWindow.this);
 			}
 		});
 		menu.add(item);
---
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/WatcherSelectorPanel.java
@@ -0,0 +1,186 @@
+package edu.jhu.cs.oose.biblio.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import edu.jhu.cs.oose.biblio.model.FileMetadata;
+import edu.jhu.cs.oose.biblio.model.Watcher;
+import edu.jhu.cs.oose.biblio.model.epub.EpubFileMetadata;
+import edu.jhu.cs.oose.biblio.model.pdf.PDFFileMetadata;
+
+/**
+ * GUI element that present newly created file in 
+ * watching directory for user to select and import
+ */
+public class WatcherSelectorPanel extends JPanel {
+
+	/** owner of this panel */
+	private WatcherSelectorDialog owner;
+	
+	/** directories that is currently watched */
+	private List<File> files;
+		
+	/** A button to open file browser to select a new directory */
+	private JButton addButton;
+	
+	/** A button to remove the selected directory */
+	private JButton removeButton;
+	
+	/** A button to close this dialog */
+	private JButton closeButton;
+	
+	/** The object that holds item for JList to display */
+	private DefaultListModel listModel;
+	
+	/** A list displaying the existing directories */
+	private JList dirList;
+	
+	/** The panel that contain dirList */
+	private JPanel listPanel;
+	
+	public WatcherSelectorPanel(List<File> files, WatcherSelectorDialog currentOwner) {
+		this.owner = currentOwner;
+		this.files = files;
+		Iterator<File> iter = files.iterator();
+		listModel = new DefaultListModel();
+		while (iter.hasNext()) {
+			listModel.addElement(iter.next().toString());
+		}
+		
+		dirList = new JList(listModel);
+		dirList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		dirList.setVisibleRowCount(-1);
+		addButton = new JButton("add");
+		addButton.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				addDir();
+			}
+		});
+		
+		removeButton = new JButton("remove");
+		removeButton.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				removeDir();
+			}
+		});
+		
+		closeButton = new JButton("close");
+		closeButton.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				closeDialog();
+			}
+		});
+		
+		listPanel = new JPanel();
+		JPanel globalOptionsPanel = new JPanel();
+		
+		listPanel.setLayout(new GridLayout(files.size(), 1));
+		globalOptionsPanel.setLayout(new GridLayout());
+		this.add(listPanel, BorderLayout.CENTER);
+		this.add(globalOptionsPanel, BorderLayout.SOUTH);
+		globalOptionsPanel.add(addButton);
+		globalOptionsPanel.add(removeButton);
+		globalOptionsPanel.add(closeButton);
+		globalOptionsPanel.setPreferredSize(new Dimension(300, 50));
+		listPanel.add(dirList);
+		listPanel.setPreferredSize(new Dimension(300, 100));
+	}
+	
+	/**
+	 * Close this dialog
+	 */
+	public void closeDialog() {
+		owner.setVisible(false);
+	}
+	
+	/**
+	 * Open file browser
+	 */
+	public void addDir() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setMultiSelectionEnabled(true);
+		fileChooser.setDialogTitle("Select directories to track");
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int choiceResult = fileChooser.showDialog(owner.getParent(), "Import");
+		if( JFileChooser.CANCEL_OPTION == choiceResult ) {
+			return;
+		}
+		else if( JFileChooser.ERROR_OPTION == choiceResult ) {
+			// TODO handle the error
+			return;
+		}
+		else if( JFileChooser.APPROVE_OPTION == choiceResult ) {
+			// get selected files
+			File[] fl = fileChooser.getSelectedFiles();
+			// add these files into JList
+			for (File f : fl) {
+				listModel.addElement(f.toString());
+			}
+			// add these files to watcher
+			Watcher w = Watcher.getWatcher();
+			w.addWatchedDirectories(Arrays.asList(fl));
+		}
+	}
+	
+	/**
+	 * Remove the selected directory, if nothing is selected, do nothing.
+	 */
+	public void removeDir() {
+		// get the selected file list
+		if (!dirList.isSelectionEmpty()) {
+			List<File> fList = new ArrayList<File>();
+			// remove these files from the JList
+			int index[] = (int[]) dirList.getSelectedIndices();
+			for (int j=0; j<index.length; j++) {
+				File fl = getFile(listModel.get(index[j]).toString());
+				if (fl != null) {
+					fList.add(fl);
+				}
+				listModel.remove(index[j]);
+			}
+			
+			// add the file list to watcher
+			Watcher w = Watcher.getWatcher();
+			w.removeWatchedDirectories(fList);
+		}
+	}
+	
+	/**
+	 * Return the file associating with the file name
+	 * @param fname the file name
+	 * 
+	 * @return the file object with the file name
+	 */
+	private File getFile(String fname) {
+		Iterator<File> iter = files.iterator();
+		while (iter.hasNext()) {
+			File fl = (File) iter.next();
+			if (fname.compareTo(fl.toString()) == 0) {
+				return fl;
+			}
+		}
+		return null;
+	}
+}
