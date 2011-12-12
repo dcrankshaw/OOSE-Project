9792cd16b3197e8d2f2e5e4073b683415f6367e1
Paul O'Neil <redballoon36@gmail.com>
2011-12-12 03:21:49 -0500

    Removed the opened count and last opened fields from the property panel

--- Biblio/src/edu/jhu/cs/oose/biblio/gui/PropertiesPanel.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/gui/PropertiesPanel.java
@@ -19,40 +19,13 @@ public class PropertiesPanel extends JPanel {
 
 	/** The file whose properties are being displayed */
 	public FileMetadata file;
-	
-	/** A label displaying the date and time of the last time this file was opened in Biblio */
-	private JLabel lastOpenedLabel;
-	/** The label displaying "Last Opened:" */
-	private JLabel lastOpenedTitle;
-	
-	/** A label displaying the number of times this file has been opened through Biblio */
-	private JLabel openedCountLabel;
-	/** The label displaying the text "Opened Count:" */ 
-	private JLabel openedCountTitle;
-	
-	/** Object that turns Dates into Strings */
-	private DateFormat formatter;
-	
+
 	/** Creates a new panel that displays the properties of nothing in particular. */
 	public PropertiesPanel() {
 		this.tagsListPane = new TagsListPanel();
-		this.lastOpenedTitle = new JLabel("Last Opened:");
-		this.lastOpenedLabel = new JLabel();
-		this.openedCountTitle = new JLabel("Opened Count:");
-		this.openedCountLabel = new JLabel();
-		
-		JPanel twoColumns = new JPanel();
-		twoColumns.setLayout(new GridLayout(2, 2));
-		twoColumns.add(this.lastOpenedTitle);
-		twoColumns.add(this.lastOpenedLabel);
-		twoColumns.add(this.openedCountTitle);
-		twoColumns.add(this.openedCountLabel);
 		
 		this.setLayout(new BorderLayout());
-		this.add(twoColumns, BorderLayout.NORTH);
 		this.add(tagsListPane, BorderLayout.CENTER);
-		
-		formatter = DateFormat.getDateInstance();
 	}
 	
 	/**
@@ -70,8 +43,6 @@ public class PropertiesPanel extends JPanel {
 	 */
 	public void setFile(FileMetadata f) {
 		this.file = f;
-		lastOpenedLabel.setText(formatter.format(this.file.getLastOpened()));
-		openedCountLabel.setText(Integer.toString(this.file.getOpenedCount()));
 		tagsListPane.setTags(this.file);
 	}
 	
