5118c76aa89122c2610674e9a9a2b0f27ec622f6
Cain <kbronze1989@hotmail.com>
2011-12-11 23:24:18 -0500

    Add unit test for FileMetadata class

--- Biblio/src/edu/jhu/cs/oose/biblio/model/tests/FileMetadataTest.java
+++ Biblio/src/edu/jhu/cs/oose/biblio/model/tests/FileMetadataTest.java
@@ -1,125 +1,123 @@
+/**
+ * 
+ */
 package edu.jhu.cs.oose.biblio.model.tests;
 
-
-import java.util.Date;
-
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 
 import org.hibernate.Session;
+import org.junit.Before;
+import org.junit.Test;
 
 import edu.jhu.cs.oose.biblio.model.Database;
 import edu.jhu.cs.oose.biblio.model.FileMetadata;
 import edu.jhu.cs.oose.biblio.model.Tag;
 
 /**
- * JUnit test class to make sure that FileMetadata works.
+ * JUnit tests on FileMetadata works.
  * @author Cain Lu
  */
-public class FileMetadataTest extends TestCase {
-	
+public class FileMetadataTest {
 	/**
 	 * The FileMetadata object to test
 	 */
-	FileMetadata file;
+	FileMetadata file, file1, file2;
 	/**
 	 * Sample tags to apply to the file.
 	 */
-	Tag tag;
+	Tag tag,tag1;
+
 	/**
-	 * Sample tags to apply to the file.
+	 * @throws java.lang.Exception
 	 */
-	Tag tag1;
-	
+	@Before
 	public void setUp() {
 		file = new TestMetadata();
+		file1 = new TestMetadata();
+		file2 = new TestMetadata();
 		Session session = Database.getNewSession();
 		session.beginTransaction();
 		try {
-			tag = new Tag("Tag");
+			tag = new Tag("Tag");			
+		
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
-			tag1 = new Tag("Tag1");
+			tag1 = new Tag("Tag1");					
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Database.commit();
-	}
-	
-	/**
-	 * Tests FileMetdata.getContents().
-	 * This is an abstract method on FileMetadata, so this is not a good test. 
-	 */
-	/*public void testGetContents() {
-		assertTrue("Get contents does not return any contents.", file.getContents() != null ); 
-	}*/
-	
-	/**
-	 * Tests FileMetadata.updateLastOpened().
-	 */
-	public void testUpdateLastOpened() {
-		Date d1 = file.getLastOpened();
-		file.setLastOpened(new Date());
-		assertTrue ("Updated LastOpened date is after now.", file.getLastOpened().after(d1));
-	}
-	
-	/**
-	 * Tests FileMetadata.getLastOpened().
-	 */
-	// TODO implement this test
-	public void testGetLastOpened(){
-		//fail("not yet implemented");
-	}
-	
-	/**
-	 * Tests FileMetadata.updateOpenCount().
-	 */
-	// TODO implement this test
-	public void testUpdateOpenCount(){
-		//fail("not yet implemented");
-	}
-	
-	/**
-	 * Tests FileMetadata.getOpenCount().
-	 */
-	// TODO implement this test
-	public void testGetOpenCount(){
-		//fail("not yet implemented");
-	}
-	
-	/**
-	 * Tests FileMetadata.setPathToFile().
-	 */
-	// TODO implement this test
-	public void testSetPathToFile(){
-		//fail("not yet implemented");
-	}
-	
-	/**
-	 * Tests FileMetadata.getPathToFile().
-	 */
-	// TODO implement this test
-	public void testGetPathToFile(){
-		//fail("not yet implemented");
-	}
-	
-	/**
-	 * Tests FileMetadata.getLastOpened().
-	 */
-	// TODO Paul:I don't think this test works...
-	public void testAddTags(){
-		assertTrue("Bad input", tag.addChild(tag1));
-	}
-	
-	/**
-	 * Test FileMetadata.getTags()
-	 */
-	// TODO implement this test
-	public void testGetTags(){
-		//fail("not yet implemented");
+		
 		
 	}
+
+
+	/**
+	 * Test method for {@link edu.jhu.cs.oose.biblio.model.FileMetadata#setId(int)} and {@link edu.jhu.cs.oose.biblio.model.FileMetadata#getId(int)}.
+	 */
+	@Test
+	public void testSetGetId() {
+		file.setId(1);	
+		assertTrue(file.getId()==1);
+	}
+
+	/**
+	 * Test method for {@link edu.jhu.cs.oose.biblio.model.FileMetadata#equals(edu.jhu.cs.oose.biblio.model.FileMetadata)}.
+	 */
+	@Test
+	public void testEqualsFileMetadata() {
+		file.setId(1);
+		file.addTag(tag);
+		file.setPathToFile("testFiles/test1.pdf");	
+		file1.setId(2);
+		file1.addTag(tag1);
+		file1.setPathToFile("testFiles/test2.pdf");		
+		file2.setId(1);
+		file2.addTag(tag);
+		file2.setPathToFile("testFiles/test1.pdf");	
+		assertTrue(file.equals(file2));
+		assertFalse(file.equals(file1));
+	}
+
+	/**
+	 * Test method for {@link edu.jhu.cs.oose.biblio.model.FileMetadata#getTags()}.
+	 */
+	@Test
+	public void testGetTags() {
+		file.addTag(tag);
+		file.addTag(tag1);
+		assertTrue(file.getTags().contains(tag)&& file.getTags().contains(tag1));		
+	}
+
+	/**
+	 * Test method for {@link edu.jhu.cs.oose.biblio.model.FileMetadata#addTag(edu.jhu.cs.oose.biblio.model.Tag)}.
+	 */
+	@Test
+	public void testAddTag() {
+		file.addTag(tag);
+		assertTrue(file.getTags().contains(tag));
+	}
+
+	/**
+	 * Test method for {@link edu.jhu.cs.oose.biblio.model.FileMetadata#SetPathToFile()} and {@link edu.jhu.cs.oose.biblio.model.FileMetadata#getPathToFile()}.
+	 */
+	@Test
+	public void testSetGetPathToFile() {
+		file.setPathToFile("testFiles/test1.pdf");
+		assertEquals(file.getPathToFile(), "testFiles/test1.pdf");
+		
+	}
+
+	/**
+	 * Test method for {@link edu.jhu.cs.oose.biblio.model.FileMetadata#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		file.setPathToFile("testFiles/test1.pdf");
+		assertEquals(file.getName(), "testFiles/test1.pdf");
+	}
+
+
 }
